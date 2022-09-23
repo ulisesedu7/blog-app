e345d10664816867f3ebf0a5e2701b438f827b5e
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-23 13:21:20 -0600

    Fix migrations and run db:migrate

--- db/migrate/20220923190452_add_author_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorIdToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :author_id, :integer
-    add_index :users, :author_id
-  end
-end
--- db/migrate/20220923190653_add_post_id_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddPostIdToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :post_id, :integer
-    add_index :posts, :post_id
-  end
-end
