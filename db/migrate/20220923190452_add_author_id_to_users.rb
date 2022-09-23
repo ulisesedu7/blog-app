dd1112a133570e6a87f8f5b7b26daac253e19a01
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-23 13:07:10 -0600

    Add indexes to foreign keys

---
+++ db/migrate/20220923190452_add_author_id_to_users.rb
@@ -0,0 +1,6 @@
+class AddAuthorIdToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :author_id, :integer
+    add_index :users, :author_id
+  end
+end
---
+++ db/migrate/20220923190653_add_post_id_to_posts.rb
@@ -0,0 +1,6 @@
+class AddPostIdToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :post_id, :integer
+    add_index :posts, :post_id
+  end
+end
