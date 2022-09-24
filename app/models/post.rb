06890d020fb683e11617d43925f01b2402a40412
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-24 10:35:07 -0600

    Set up data validation in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post < ApplicationRecord
+  # Data Relationships
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+  
+  # Data Validation
+  validates :title, presence: true, length { in: 1..250 }
+  validates :comments_counter, presence: true, numerically: { greater_than_or_equal_to: 0, only_integer: trye}
+  validates :likes_counter, presence: true, numerically: { greater_than_or_equal_to: 0, only_integer: trye}
 
   after_save :update_posts_counter
+
 
   def recent_comments
     Comment.where(post: self).order(updated_at: :desc).first(5)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  # Data Relationships
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+
+  # Data Validation
+  validates :title, presence: true
+  validates :post_counter, presence: true, numerically: { greater_than_or_equal_to: 0, only_integer: trye}
 
   def recent_posts
     Post.where(author: self).order(updated_at: :desc).first(3)
