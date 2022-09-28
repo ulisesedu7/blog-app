1af880c152dbe4782f16db24985267e230cc04b0
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-28 00:39:45 -0600

    Test routes to make sure everything is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  # root "users#index"
 
   # Posts Routes
   get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts', to: 'posts#show'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
 
   # Users Routes
   get 'users/', to: 'users#index'
