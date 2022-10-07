989fe9db7036f05864dde3690201717150bce7e4
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-10-07 10:09:03 -0600

    Display initial data and initialize controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # root "users#index"
 
   # Posts Routes
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
+  get 'users/:user_id/posts', to: 'posts#index', as: :posts
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: :post
 
   # Users Routes
   get 'users/', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: :user
 end
